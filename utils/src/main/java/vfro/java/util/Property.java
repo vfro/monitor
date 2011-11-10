01621080fc7150c5467ad3485a9f39d5a42592d8
Volodymyr Frolov <frolov.volodymyr@gmail.com>
2011-11-10 19:53:44 +0200

    implementation of Property equals and hashCode methods

--- utils/src/main/java/vfro/java/util/Property.java
+++ utils/src/main/java/vfro/java/util/Property.java
@@ -14,4 +14,25 @@ public class Property<Value> {
 	public void set(Value value) {
 		this.value = value;
 	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this.value == null) {
+			return false;
+		} else if (object == null) {
+			return false;
+		} else if (this == object) {
+			return true;
+		} else if (object.getClass() != this.getClass()) {
+			return false;
+		}
+
+		Property property = (Property)object;
+		return value.equals(property.get());
+	}
+
+	@Override
+	public int hashCode() {
+		return value.hashCode();
+	}
 }
--- utils/src/test/java/vfro/util/PropertyTest.java
+++ utils/src/test/java/vfro/util/PropertyTest.java
@@ -2,6 +2,8 @@ package vfro.java.util;
 
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class PropertyTest {
@@ -23,5 +25,68 @@ public class PropertyTest {
 		stringProperty.set(stringValue);
 
 		assertTrue(stringValue == stringProperty.get(), "Test Property.get() method with value passed into Property.set().");
+	}
+
+	@Test
+	public void propertySameValuesEquals() {
+		Property<String> stringProperty1 = new Property<String>("Property");
+		Property<String> stringProperty2 = new Property<String>("Property");
+
+		assertTrue(stringProperty1.equals(stringProperty2), "Test Property.equals() method with the same values.");
+	}
+
+	@Test
+	public void propertyDifferentValuesEquals() {
+		Property<String> stringProperty1 = new Property<String>("Property 1");
+		Property<String> stringProperty2 = new Property<String>("Property 2");
+
+		assertFalse(stringProperty1.equals(stringProperty2), "Test Property.equals() method with different values.");
+	}
+
+	@Test
+	public void propertyDifferentPropertyClassesEquals() {
+		Property<String> stringProperty1 = new Property<String>("Property");
+		Property<String> stringProperty2 = new Property<String>("Property") {};
+
+		assertFalse(stringProperty1.equals(stringProperty2), "Test Property.equals() method with different classes of properties.");
+	}
+
+	private static class Object1 {
+		@Override
+		public boolean equals(Object object) {
+			return true;
+		}
+	}
+
+	private static class Object2 {
+		@Override
+		public boolean equals(Object object) {
+			return true;
+		}
+	}
+
+	@Test
+	public void propertyDifferentValueClassesEquals() {
+		Property<Object1> property1 = new Property<Object1>(new Object1());
+		Property<Object2> property2 = new Property<Object2>(new Object2());
+
+		assertTrue(property1.equals(property2), "Test Property.equals() method with different classes of values.");
+	}
+
+	@Test
+	public void propertyNullEquals() {
+		Property<String> stringProperty1 = new Property<String>(null);
+		Property<String> stringProperty2 = new Property<String>("Property");
+
+		assertFalse(stringProperty1.equals(stringProperty2), "Test Property.equals() method with null owner value.");
+		assertFalse(stringProperty2.equals(null), "Test Property.equals() method with null parameter value.");
+		assertFalse(stringProperty2.equals(stringProperty1), "Test Property.equals() method with null parameter property value.");
+	}
+
+	@Test
+	public void propertyHashCode() {
+		String value = new String("Property");
+		Property<String> property = new Property<String>(value);
+		assertEquals(property.hashCode(), value.hashCode());
 	}
 }
