25b5534405a05583663b8df066231c469e4936c3
Volodymyr Frolov <frolov.volodymyr@gmail.com>
2011-11-11 12:20:39 +0200

    javadoc for IFluentProperty interface

--- c3h8/src/main/java/c3h8/java/util/IFluentProperty.java
+++ c3h8/src/main/java/c3h8/java/util/IFluentProperty.java
@@ -1,5 +1,19 @@
 package c3h8.java.util;
 
+/**
+ * The aim of IFluentProperty interface is to unify {@code FluentProperty} and
+ * {@code FluentDelegateProperty} classes into one hierarchy.
+ */
 public interface IFluentProperty<Owner, Value> {
+	/**
+	 * Set a new {@code value} for a property and link owner fluently.<p>
+	 * Example:<p>
+	 * <pre>
+	 * Person.lastName.with("Smith")
+	 *       .firstName.with("John");
+	 * </pre>
+	 * @param value new value for a property.
+	 * @return link to property owner object.
+	 */
 	Owner with(Value value);
 }
