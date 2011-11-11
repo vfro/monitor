0a0bb5cdeec0e9f030463ab50547d9a06ba7da40
Volodymyr Frolov <frolov.volodymyr@gmail.com>
2011-11-11 10:34:47 +0200

    fixed javadoc generation for propane project

--- c3h8/src/main/java/c3h8/java/util/Property.java
+++ c3h8/src/main/java/c3h8/java/util/Property.java
@@ -1,5 +1,35 @@
 package c3h8.java.util;
 
+/**
+ * Property base class. {@code Property} instances should be
+ * exposed as a public final fields of a class. It gives potential
+ * ability to override set/get methods later but avoid defining
+ * (and thus testing) getters and setters of each in each class.<p>
+ * <pre>
+ * public class Person {
+ *    // Property with overrided setter and getter.
+ *    public final Property<String> firstName = new Property<String>("") {
+ *       &#064;Override
+ *       public String get() {
+ *          System.out.println("Value of property Person.firstName has been accessed.");
+ *          return super.get();
+ *       }
+ *
+ *       &#064;Override
+ *       public void set(String value) {
+ *          super.set(value);
+ *          System.out.println("Value of property Person.firstName has been changed.");
+ *       }
+ *    };
+ *
+ *    // Properties without overrided setter and getter, but with ability to define them later.
+ *    public final Property<String> lastName = new Property<String>("");
+ *    public final Property<Calendar> dateOfBirth = new Property<Calendar>(Calendar.getInstance());
+ * }
+ * </pre>
+ *
+ * @author Volodymyr Frolov <frolov.volodymyr@gmail.com>
+ */
 public class Property<Value> {
 	private Value value;
 
