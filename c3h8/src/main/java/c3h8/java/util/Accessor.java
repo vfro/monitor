476ca3b4d629849e22c8e0e28b32f28aa3bb147e
Volodymyr Frolov <frolov.volodymyr@gmail.com>
2011-11-15 19:05:06 +0200

    javadocs for accessor and checker

--- c3h8/src/main/java/c3h8/java/util/Accessor.java
+++ c3h8/src/main/java/c3h8/java/util/Accessor.java
@@ -1,9 +1,22 @@
 package c3h8.java.util;
 
 /**
- * Accessor is an interface which is used by monitor to access
- * its value which is cannot be directly returned from getter.
+ * Accessor is an interface which is used by {@link Monitor} to access
+ * its value which cannot be directly returned from getter.
  */
 public interface Accessor<Value> {
+	/**
+	 * Override this method to access the value of the monitor.
+	 * When this method is called either shared or exclusive synchronization
+	 * was already done.
+	 * @param value The value of the monitor to access. This method should change
+	 * the internal state of {@code value} only in case of write monitor access.
+	 * In this case {@code value} should be used as a return value anyway.
+	 * @return In case of write access the returned value will become a new value
+	 * of a monitor. If write accessor changed the internal state of {@code value}
+	 * object, it must return this parameter anyway.
+	 * In case of read access the returned value is ingored and may be either {@code value}
+         * or null.
+	 */
 	Value access(Value value);
 }
--- c3h8/src/main/java/c3h8/java/util/Checker.java
+++ c3h8/src/main/java/c3h8/java/util/Checker.java
@@ -1,5 +1,21 @@
 package c3h8.java.util;
 
+/**
+ * Checker to determinate if {@link Monitor} value becomed to appropriate state for access.
+ * In some cases {@link Accessor} should acquire access to a value of a {@link Monitor} only
+ * after the value becomed into some certain state. A state of the {@link Monitor} value may
+ * be checked by instances of this interface.
+ * Monitor will call {@link #check} method each time when its value is changed
+ * and allow access of its value only when {@code check} method returned {@code true}.
+ */
 public interface Checker<Value> {
+	/**
+	 * Check if a value of a {@link Monitor} becomed to appropriate state for access.
+         * @param value. Value of a {@link Monitor} to check. Checker must not change internal
+	 * state of a {@code value} object. Otherwise all other accessors won't be able to track
+	 * changes and wake up.
+	 * @return {@code true} if {@link Monitor} becomed to appropriate state for access.
+	 * Otherwise {@code false}.
+	 */
 	boolean check(Value value);
 }
