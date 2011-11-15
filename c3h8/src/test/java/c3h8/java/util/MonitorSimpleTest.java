78d699d3a63bece9769b96219c194d493b5442a3
Volodymyr Frolov <frolov.volodymyr@gmail.com>
2011-11-15 22:51:33 +0200

    Monitor nanoseconds and time unit interrupted wait test

--- c3h8/src/test/java/c3h8/java/util/MonitorSimpleTest.java
+++ c3h8/src/test/java/c3h8/java/util/MonitorSimpleTest.java
@@ -1,8 +1,12 @@
 package c3h8.java.util;
 
+import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.fail;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
 
 public class MonitorSimpleTest {
 	public MonitorSimpleTest() {
@@ -60,18 +64,18 @@ public class MonitorSimpleTest {
 		final String modifiedValue = "modified";
 		Monitor<String> monitor = new Monitor<String>(initialValue);
 
-		monitor.writeAccess(new Accessor<String>() {
-			@Override
-			public String access(String value) {
-				return modifiedValue;
-			}
-		}, new Checker<String>() {
-			@Override
-			public boolean check(String value) {
-				return value == initialValue;
-			}
-		}
-		);
+		monitor.writeAccess(
+			new Accessor<String>() {
+				@Override
+				public String access(String value) {
+					return modifiedValue;
+				}
+			}, new Checker<String>() {
+				@Override
+				public boolean check(String value) {
+					return value == initialValue;
+				}
+			});
 
 		monitor.readAccess(new Accessor<String>() {
 			@Override
@@ -80,5 +84,81 @@ public class MonitorSimpleTest {
 				return null;
 			}
 		});
+	}
+
+	@Test
+	public void monitorNanosecondsReadTest() throws InterruptedException {
+		Monitor<String> monitor = new Monitor<String>(null);
+		long result = monitor.readAccess(
+			new Accessor<String>() {
+				@Override
+				public String access(String value) {
+					fail("Never reach read access for false checer.");
+					return null;
+				}
+			}, new Checker<String>() {
+				@Override
+				public boolean check(String value) {
+					return false;
+				}
+			}, 1000);
+		assertTrue(result <= 0, "Test Monitor.readAccess interrupt wait for nanoseconds.");
+	}
+
+	@Test
+	public void monitorNanosecondsWriteTest() throws InterruptedException {
+		Monitor<String> monitor = new Monitor<String>(null);
+		long result = monitor.writeAccess(
+			new Accessor<String>() {
+				@Override
+				public String access(String value) {
+					fail("Never reach write access for false checer.");
+					return null;
+				}
+			}, new Checker<String>() {
+				@Override
+				public boolean check(String value) {
+					return false;
+				}
+			}, 1000);
+		assertTrue(result <= 0, "Test Monitor.writeAccess interrupt wait for nanoseconds.");
+	}
+
+	@Test
+	public void monitorTimeunitReadTest() throws InterruptedException {
+		Monitor<String> monitor = new Monitor<String>(null);
+		boolean result = monitor.readAccess(
+			new Accessor<String>() {
+				@Override
+				public String access(String value) {
+					fail("Never reach read access for false checer.");
+					return null;
+				}
+			}, new Checker<String>() {
+				@Override
+				public boolean check(String value) {
+					return false;
+				}
+			}, 10, TimeUnit.MILLISECONDS);
+		assertFalse(result, "Test Monitor.readAccess interrupt wait for milliseconds.");
+	}
+
+	@Test
+	public void monitorTimeunitWriteTest() throws InterruptedException {
+		Monitor<String> monitor = new Monitor<String>(null);
+		boolean result = monitor.writeAccess(
+			new Accessor<String>() {
+				@Override
+				public String access(String value) {
+					fail("Never reach write access for false checer.");
+					return null;
+				}
+			}, new Checker<String>() {
+				@Override
+				public boolean check(String value) {
+					return false;
+				}
+			}, 10, TimeUnit.MILLISECONDS);
+		assertFalse(result, "Test Monitor.writeAccess interrupt wait for milliseconds.");
 	}
 }
