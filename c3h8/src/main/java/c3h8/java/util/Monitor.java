c420ac7b07450bbbeb54c0f0d69437bd8a7b829f
Volodymyr Frolov <frolov.volodymyr@gmail.com>
2011-11-15 15:09:07 +0200

    added await time, nanos and date to Monitor

--- c3h8/src/main/java/c3h8/java/util/Monitor.java
+++ c3h8/src/main/java/c3h8/java/util/Monitor.java
@@ -1,5 +1,7 @@
 package c3h8.java.util;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
@@ -63,6 +65,59 @@ public class Monitor<Value> {
 		}
 	}
 
+	public boolean readAccess(Accessor<Value> accessor, Checker<Value> checker, long time, TimeUnit unit)
+		throws InterruptedException {
+		try {
+			this.rlock.lock();
+			while (!checker.check(this.value)) {
+				if (!rtouch.await(time, unit)) {
+					return false;
+				}
+			}
+			accessor.access(this.value);
+		}
+		finally {
+			this.rlock.unlock();
+		}
+		return true;
+	}
+
+	public long readAccess(Accessor<Value> accessor, Checker<Value> checker, long nanosTimeout)
+		throws InterruptedException {
+		long result = 0;
+		try {
+			this.rlock.lock();
+			while (!checker.check(this.value)) {
+				result = rtouch.awaitNanos(nanosTimeout);
+				if (result <= 0) {
+					return result;
+				}
+			}
+			accessor.access(this.value);
+		}
+		finally {
+			this.rlock.unlock();
+		}
+		return result;
+	}
+
+	public boolean readAccess(Accessor<Value> accessor, Checker<Value> checker, Date deadline)
+		throws InterruptedException {
+		try {
+			this.rlock.lock();
+			while (!checker.check(this.value)) {
+				if (!rtouch.awaitUntil(deadline)) {
+					return false;
+				}
+			}
+			accessor.access(this.value);
+		}
+		finally {
+			this.rlock.unlock();
+		}
+		return true;
+	}
+
 	public void writeAccess(Accessor<Value> accessor, Checker<Value> checker)
 		throws InterruptedException {
 		try {
@@ -76,6 +131,62 @@ public class Monitor<Value> {
 		finally {
 			this.wlock.unlock();
 		}
+	}
+
+	public boolean writeAccess(Accessor<Value> accessor, Checker<Value> checker, long time, TimeUnit unit)
+		throws InterruptedException {
+		try {
+			this.wlock.lock();
+			while (!checker.check(this.value)) {
+				if (!wtouch.await(time, unit)) {
+					return false;
+				}
+			}
+			this.value = accessor.access(this.value);
+			this.touch();
+		}
+		finally {
+			this.wlock.unlock();
+		}
+		return true;
+	}
+
+	public long writeAccess(Accessor<Value> accessor, Checker<Value> checker, long nanosTimeout)
+		throws InterruptedException {
+		long result = 0;
+		try {
+			this.wlock.lock();
+			while (!checker.check(this.value)) {
+				result = wtouch.awaitNanos(nanosTimeout);
+				if (result <= 0) {
+					return result;
+				}
+			}
+			this.value = accessor.access(this.value);
+			this.touch();
+		}
+		finally {
+			this.wlock.unlock();
+		}
+		return result;
+	}
+
+	public boolean writeAccess(Accessor<Value> accessor, Checker<Value> checker, Date deadline)
+		throws InterruptedException {
+		try {
+			this.wlock.lock();
+			while (!checker.check(this.value)) {
+				if (!wtouch.awaitUntil(deadline)) {
+					return false;
+				}
+			}
+			this.value = accessor.access(this.value);
+			this.touch();
+		}
+		finally {
+			this.wlock.unlock();
+		}
+		return true;
 	}
 
 	public void writeAccess(Accessor<Value> accessor) {
